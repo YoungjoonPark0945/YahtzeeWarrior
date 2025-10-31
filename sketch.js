2b214ed792860ebfc45fa7494ebecdf1d01d18b4
Youngjoon Park <youngjoon@vt.edu>
2025-10-31 14:33:53 -0400

    5 Push

--- sketch.js
+++ sketch.js
@@ -107,12 +107,12 @@ function drawCharacters() {
 
   // Draw Warrior
   if (warriorImg) {
-    image(warriorImg, width / 2 - 180, height / 2 + 150, 50, 70);
+    image(warriorImg, width / 2 - 180, height / 2 + 220, 20, 10);
   } 
 
   // Draw Monster
   if (monsterImg) {
-    image(monsterImg, width / 2 + 100, height / 2 + 100, 200, 240);
+    image(monsterImg, width / 2, height / 2 + 100, 200, 240);
   }
 }
 
